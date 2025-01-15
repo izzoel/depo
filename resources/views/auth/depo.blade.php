8085a7c6b74f2453c5305df28b11031ff12f3d65
izzoel <ramadhani.zulfahmi@gmail.com>
2025-01-16 00:36:29 +0800

    - hide button version

--- resources/views/auth/depo.blade.php
+++ resources/views/auth/depo.blade.php
@@ -89,7 +89,7 @@
     <div class="layout-overlay layout-menu-toggle"></div>
     </div>
     <!-- / Layout wrapper -->
-
+    {{-- 
     <div class="buy-now">
         <button type="button" class="btn btn-danger btn-buy-now" data-bs-toggle="modal" data-bs-target="#betaModal">Beta v2.0</button>
 
@@ -140,7 +140,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     @include('auth.layout.footer')
 
