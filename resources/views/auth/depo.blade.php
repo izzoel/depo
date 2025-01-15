7cfed3d9a7924e0809cff1679cd14e5e47436387
izzoel <ramadhani.zulfahmi@gmail.com>
2025-01-16 00:03:13 +0800

    + footer & version detail

--- resources/views/auth/depo.blade.php
+++ resources/views/auth/depo.blade.php
@@ -34,22 +34,15 @@
                 <!-- Footer -->
                 <footer class="content-footer footer bg-footer-theme">
                     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-                        <div class="mb-2 mb-md-0">
-                            ©
-                            <script>
-                                document.write(new Date().getFullYear());
-                            </script>
-                            , made with ❤️ by
-                            <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
-                        </div>
-                        <div>
+                        <div class="mb-2 mb-md-0">developed by <a href="https://izzoel.github.io/" class="footer-link fw-bolder">zetware.id</a> @2025</div>
+                        {{-- <div>
                             <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
                             <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
 
                             <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a>
 
                             <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank" class="footer-link me-4">Support</a>
-                        </div>
+                        </div> --}}
                     </div>
                 </footer>
                 <!-- / Footer -->
@@ -97,7 +90,55 @@
     <!-- / Layout wrapper -->
 
     <div class="buy-now">
-        <a href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/" target="_blank" class="btn btn-danger btn-buy-now">Upgrade to Pro</a>
+        <button type="button" class="btn btn-danger btn-buy-now" data-bs-toggle="modal" data-bs-target="#betaModal">Beta v2.0</button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="betaModal" tabindex="-1" aria-labelledby="betaModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="betaModalLabel">Beta v2.0</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-text">
+                                    <table class="table table-responsive fst-italic">
+                                        <tbody>
+                                            <tr>
+                                                <td>Release Date</td>
+                                                <td>--</td>
+                                                <td>15 Januari 2025 23:29</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Penggembang</td>
+                                                <td>--</td>
+                                                <td>zetware.id</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="align-top">Fitur</td>
+                                                <td class="align-top">--</td>
+                                                <td class="align-top">
+                                                    >responsive (web, tablet, mobile)<br>
+                                                    >optimasi performa query data<br>
+                                                    >interaktif chart statistik<br>
+                                                    >profil pengguna<br>
+                                                    >ganti password
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     @include('auth.layout.footer')
