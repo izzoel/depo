5664826a1d570929bfd12c4ae79ff5b7cfdcf69a
izzoel <ramadhani.zulfahmi@gmail.com>
2025-01-16 00:50:39 +0800

    - fix favico

--- resources/views/auth/layout/header.blade.php
+++ resources/views/auth/layout/header.blade.php
@@ -6,7 +6,7 @@
   <meta name="description" content="" />
 
   <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon/favicon.ico') }}" />
+  <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}" />
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
